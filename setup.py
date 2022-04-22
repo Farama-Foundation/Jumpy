a1832ad526c95c96e6e22d307a9bd071c7f26192
StringTheory <mark.m.towers@gmail.com>
2022-04-22 16:51:13 +0100

    Update version number to 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name="brax-jumpy",
-    version="0.0.1",
+    version="0.0.2",
     description=("Common backend for JAX or numpy."),
     author="Brax Authors",
     author_email="no-reply@google.com",
