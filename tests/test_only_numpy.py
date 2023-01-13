826b0efbb72ccfef19520e04ccbbd449bd2e0289
Bas van der Heijden <d.s.vanderheijden@tudelft.nl>
2023-01-13 16:35:43 +0100

    fix cond

--- tests/test_only_numpy.py
+++ tests/test_only_numpy.py
@@ -169,7 +169,7 @@ def test_custom_np_ops_func(func_name, kwargs, expected):
 
 def test_meshgrid_cond():
     """Test that meshgrid and cond work as use `*operands` this doesn't work with `test_custom_np_func`."""
-    out = jumpy.lax.cond(True, lambda x: x[0] + 1, lambda x: x[0] - 1, 5)
+    out = jumpy.lax.cond(True, lambda *x: x[0] + 1, lambda *x: x[0] - 1, 5)
     expected_out = 6
     assert out == expected_out
 
